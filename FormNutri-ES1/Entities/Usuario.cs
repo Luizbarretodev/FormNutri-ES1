ac80a5907773e6e68b2de4bca097a03c53149960
Luizbarretodev <gugajoao1020@gmail.com>
2026-09-01 14:50:51 -0300

    feat: adiciona entities

---
+++ FormNutri-ES1/Entities/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using FormNutri_ES1.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace FormNutri_ES1.Entities;
+
+public class Usuario
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Nome { get; set; } = string.Empty;
+
+    [Required]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string Senha { get; set; } = string.Empty;
+
+    [Required]
+    public PerfilUsuario Perfil { get; set; }
+
+}
