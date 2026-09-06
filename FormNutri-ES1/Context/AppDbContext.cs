7787ef3de70f9fea25aea00c770b366e498d958e
Luizbarretodev <gugajoao1020@gmail.com>
2026-09-06 17:15:59 -0300

    feat: add AppDbContext p/ inserir registros na database

---
+++ FormNutri-ES1/Context/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using FormNutri_ES1.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FormNutri_ES1.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Usuario> Usuarios => Set<Usuario>();
+    public DbSet<RegistroAvaliacao> RegistroAvaliacoes => Set<RegistroAvaliacao>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<RegistroAvaliacao>(builder =>
+        {
+            builder.OwnsOne(r => r.Respondente);
+            builder.OwnsOne(r => r.RespostaEbia);
+
+            builder.OwnsOne(r => r.MarcadorConsumo, marcador =>
+            {
+                marcador.Property(m => m.RefeicoesRealizadas)
+                    .HasConversion(
+                        lista => string.Join(',', lista),
+                        texto => texto.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList()
+                    );
+            });
+
+            builder.HasOne<Usuario>()
+                .WithMany()
+                .HasForeignKey(r => r.AplicadorId);
+        });
+
+        modelBuilder.Entity<Usuario>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+    }
+}
