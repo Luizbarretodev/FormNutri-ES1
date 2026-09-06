865dfc68d6883a478a3648e66065bd960b162e2d
Luizbarretodev <gugajoao1020@gmail.com>
2026-09-06 16:39:41 -0300

    feat: add entidade MarcadorConsumo

---
+++ FormNutri-ES1/Entities/MarcadorConsumo.cs
@@ -0,0 +1,17 @@
+﻿using FormNutri_ES1.Enums;
+
+namespace FormNutri_ES1.Entities;
+
+public class MarcadorConsumo
+{
+    public List<string> RefeicoesRealizadas { get; set; }
+    public Resposta UsoTelaRefeicoes { get; set; } = new();
+    public Resposta ConsumoFeijao { get; set; } = new();
+    public Resposta ConsumoFrutas { get; set; } = new();
+    public Resposta ConsumoVerduras { get; set; } = new();
+    public Resposta ConsumoEmbutidos { get; set; } = new();
+    public Resposta ConsumoBebidasAdocadas { get; set; } = new();
+    public Resposta ConsumoMiojo { get; set; } = new();
+    public Resposta ConsumoDoces { get; set; } = new();
+
+}
