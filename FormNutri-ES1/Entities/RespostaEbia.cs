7e5891304370d0ab3524f4347ace1f85d84d3c33
Luizbarretodev <gugajoao1020@gmail.com>
2026-09-06 16:41:28 -0300

    feat: add entidade RespostaEbia

---
+++ FormNutri-ES1/Entities/RespostaEbia.cs
@@ -0,0 +1,15 @@
+﻿using FormNutri_ES1.Enums;
+
+namespace FormNutri_ES1.Entities;
+
+public class RespostaEbia
+{
+    public Resposta Pergunta1 { get; set; } = new();
+    public Resposta Pergunta2 { get; set; } = new();
+    public Resposta Pergunta3 { get; set; } = new();
+    public Resposta Pergunta4 { get; set; } = new();
+    public Resposta Pergunta5 { get; set; } = new();
+    public Resposta Pergunta6 { get; set; } = new();
+    public Resposta Pergunta7 { get; set; } = new();
+    public Resposta Pergunta8 { get; set; } = new();
+}
