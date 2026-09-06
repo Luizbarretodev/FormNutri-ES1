d460374ad5df8bcdb042b3998293b0a36d53d064
Luizbarretodev <gugajoao1020@gmail.com>
2026-09-06 16:20:49 -0300

    feat: add entidade Respondente

---
+++ FormNutri-ES1/Entities/Respondente.cs
@@ -0,0 +1,14 @@
+﻿namespace FormNutri_ES1.Entities;
+
+public class Respondente
+{
+    public string Nome { get; set; } = string.Empty;
+    public int Idade { get; set; }
+    public string Genero { get; set; } = string.Empty;
+    public string RacaCor { get; set; } = string.Empty;
+    public string Escolaridade { get; set; } = string.Empty;
+    public string EstadoCivil { get; set; } = string.Empty;
+    public string SituacaoEmprego { get; set; } = string.Empty;
+    public bool RecebeBolsaFamilia { get; set; }
+    public string Religiao { get; set; } = string.Empty;
+}
