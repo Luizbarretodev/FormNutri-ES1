aa38204ac0adf5c6e3578e4df848f79409cc73d8
Luizbarretodev <gugajoao1020@gmail.com>
2026-09-07 19:50:37 -0300

    chore: add migration para alterar campo Dependentes

--- FormNutri-ES1/Migrations/20260907221736_AjustaCamposRespondente.Designer.cs
+++ FormNutri-ES1/Migrations/20260907221736_AjustaCamposRespondente.Designer.cs
@@ -4,6 +4,7 @@ using FormNutri_ES1.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FormNutri_ES1.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260907221736_AjustaCamposRespondente")]
+    partial class AjustaCamposRespondente
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FormNutri-ES1/Migrations/20260907221736_AjustaCamposRespondente.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FormNutri_ES1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjustaCamposRespondente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Respondente_Dependentes",
+                table: "RegistroAvaliacoes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Respondente_Dependentes",
+                table: "RegistroAvaliacoes");
+        }
+    }
+}
