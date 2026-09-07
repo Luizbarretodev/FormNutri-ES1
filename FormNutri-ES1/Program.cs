92b583fe5456e46e3fbcb02939189eb94b40394d
Luizbarretodev <gugajoao1020@gmail.com>
2026-09-07 18:41:00 -0300

    chore: adiciona serviço do EbiaCalcuation no program.cs

--- FormNutri-ES1/Program.cs
+++ FormNutri-ES1/Program.cs
@@ -1,4 +1,5 @@
 using FormNutri_ES1.Context;
+using FormNutri_ES1.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<EbiaCalculationService>();
 
 var app = builder.Build();
 
